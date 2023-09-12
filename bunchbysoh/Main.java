c5248afa58bb403cc891a8efa28cc73a864c4cf2
dhiresh2003 <97101696+dhiresh2003@users.noreply.github.com>
2023-09-12 13:06:18 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,38 +1,57 @@
 package bunchbysoh;
 
 public class Main {
-  static class CountsBySoH {
-    public int healthy = 0;
-    public int exchange = 0;
-    public int failed = 0;
-  };
-
-  static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
-    CountsBySoH counts = new CountsBySoH();
-    for (int capacity : presentCapacities) {
-      double soh = 100.0 * capacity / 120.0; 
-      if (soh > 80.0 && soh <= 100.0) {
-        counts.healthy++;
-      } else if (soh >= 63.0 && soh <= 80.0) {
-        counts.exchange++;
-      } else if (soh < 63.0) {
-        counts.failed++;
-      }
+    static class CountsBySoH {
+        public int healthy = 0;
+        public int exchange = 0;
+        public int failed = 0;
     }
-    return counts;
-  }
+    static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
+        CountsBySoH counts = new CountsBySoH();
 
-  static void testBucketingByHealth() {
-    System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {115, 118, 80, 95, 91, 72};
-    CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-    assert(counts.healthy == 2);
-    assert(counts.exchange == 3);
-    assert(counts.failed == 1);
-    System.out.println("Done counting :)\n");
-  }
+        for (int capacity : presentCapacities) {
+            double stateOfHealth = calculateSoH(capacity);
 
-  public static void main(String[] args) {
-    testBucketingByHealth();
-  }
+            if (isHealthy(stateOfHealth)) {
+                counts.healthy++;
+            } else if (isExchange(stateOfHealth)) {
+                counts.exchange++;
+            } else {
+                counts.failed++;
+            }
+        }
+        return counts;
+    }
+    private static double calculateSoH(int presentCapacity) {
+        final int ratedCapacity = 120;
+        return 100.0 * presentCapacity / ratedCapacity;
+    }
+    private static boolean isHealthy(double soh) {
+        return soh > 80.0;
+    }
+    private static boolean isExchange(double soh) {
+        return soh >= 63.0 && soh <= 80.0;
+    }
+    static void testBucketingByHealth() {
+        System.out.println("Counting batteries by SoH...\n");
+        int[] presentCapacities = {115, 118, 80, 95, 91, 72};
+        CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+        assert(counts.healthy == 2);
+        assert(counts.exchange == 3);
+        assert(counts.failed == 1);
+        System.out.println("Done counting :)\n");
+    }
+    static void testBoundaryConditions() {
+        System.out.println("Testing boundary conditions...\n");
+        int[] presentCapacities = {96, 76};
+        CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+        assert(counts.healthy == 0);
+        assert(counts.exchange == 2);
+        assert(counts.failed == 0);
+        System.out.println("Boundary conditions tested :)\n");
+    }
+    public static void main(String[] args) {
+        testBucketingByHealth();
+        testBoundaryConditions();
+    }
 }
